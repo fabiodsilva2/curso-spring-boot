a9dc52037504efc088f8dc40b18e6ec14837f64d
Fábio Damasceno Silva <fabiodamasceno4@hotmail.com>
2022-08-01 09:36:26 -0300

    Fechada a aula de cadastrar um tópico

--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -30,7 +30,15 @@ public class Topico {
 
 	
 	
-
+	public Topico(String titulo, String mensagem, Curso curso) {
+		super();
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
+	public Topico() {
+		
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
