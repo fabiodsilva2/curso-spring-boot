e1d4e4e3ede9ea7930121bd2ee0c8606615b8e8f
Fábio Damasceno Silva <fabiodamasceno4@hotmail.com>
2022-08-28 12:29:57 -0300

    Deploy em ambiente de produção

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,16 +2,25 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport//Para o Spring pegar as informações de paginação http://localhost:8080/topicos?page=0&size=10&sort=id,desc&sort=dataCriacao,asc
 @EnableCaching
-public class ForumApplication {
+public class ForumApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(ForumApplication.class);
+	}
+	
 
 }
